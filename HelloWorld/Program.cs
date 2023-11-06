2b7dd83327f52e80b4ac19bea84af7ba17ee95b2
84918 <84918@DESKTOP-DJ354RH>
2023-11-06 23:30:01 +0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String myname = "Ninh Ngoc Son";
+            Console.WriteLine("My name is " + myname);
+            
+        }
+    }
+}
