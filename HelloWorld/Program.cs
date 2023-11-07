9e5790ca3c08bb7415dd1bb567793454be9d39e5
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-07 15:57:25 +0700

    Bai 9: Tham tri , tham chieu

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 namespace HelloWorld
 {
+    class Student
+    {
+        public string StudentName { set; get; }
+    }
     internal class Program
     {
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Barry";
+        }
+        static void ChangeValue(int x)                        
+        {
+            x = 3;
+            Console.WriteLine("X = "+x);    
+        }
         static void Main(string[] args)
         {
-            String myname = "Ninh Ngoc Son";
-            Console.WriteLine("My name is " + myname);
-            float floatNumber1= 3.658f;
-            
+            Student std1 = new Student();
+            std1.StudentName = "Shark";
+            ChangeReferenceType(std1);
+            Console.WriteLine(std1.StudentName);
+            //int i = 5;
+            ////Console.WriteLine(i);
+            //ChangeValue(i);
+            ////Console.WriteLine("I = "+i);
+            Console.Read();
         }
     }
 }
