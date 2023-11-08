93a578d1f26c5091b1350bf3e00955a228848037
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-08 10:32:52 +0700

    Bai11: Chuyen doi kieu du lieu

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,36 +7,43 @@ namespace HelloWorld
         
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Print on ");   //in chuỗi xuông dòng
-            Console.WriteLine("New Line ");
-            Console.Write("Print on ");       //in chuỗi bình thường, không xuống
-            Console.Write("Same line");
-            Console.WriteLine();
-            int value = 10;
-            //Variable
-            Console.WriteLine(value);
-            //Literal
-            Console.WriteLine(50.05);
-            Console.WriteLine("Hello " + "World"); //kết nối chuỗi bằng dấu +
-            Console.WriteLine("Value = " + value);
-            Console.WriteLine("Value = {0}", value);//Sử dụng Fomatted string giữa chỗ cho biến
-            int firstNumber = 6; int seccondNumber = 4; int result = firstNumber+seccondNumber;
-            Console.WriteLine("{0} + {1} = {2}", firstNumber, seccondNumber, result); // mỗi một biến được sử dụng sẽ có một placeholder
-            */
-            string testString;
-            Console.Write("Enter a string - ");
-            testString = Console.ReadLine();// ReadLine() đọc dòng tiếp theo của input
-            Console.WriteLine("You ented '{0}'", testString);
+            int myInt = 5;
+            double myDouble = myInt;//Sử dụng Implicit Casting chuyển đổi kiểu dữ liệu nhỏ hơn sang kiểu dữ liệu lớn hơn
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+            //Sử dụng Explicit chuyển đổi kiểu dữ liệu lớn hơn sang kiểu dữ liệu nhỏ hơn
+            //Casting chuyển 1 giá trị từ kiểu này sang kiểu khác hoặc xuất ra lỗi
+            int five = 6;
+            var doubleFive = (double)five;
+            Console.WriteLine(five);
+            Console.WriteLine(doubleFive);
+            char a = 'l';
+            var valuaA = (int)a;
+            Console.WriteLine(valuaA);
+            float myFloat = 3.27f;
+            decimal myDecimal = (decimal)myFloat;
+            Console.WriteLine(myDecimal);
+            //Conversion chuyển một kiểu đối tượng sang kiểu khác, ít lỗi hơn nhưng chậm hơn
+            decimal deFive = Convert.ToDecimal(five);
+            Console.WriteLine(deFive);
+            decimal myMoney = 4.48M;
+            int myMoney1 = Convert.ToInt32(myMoney);
+            Console.WriteLine(myMoney1);
+            //Parsing chuyển một chuỗi sang kiểu nguyên thủy
+            string testString = "102023";
+            int intValue = int.Parse(testString);
+            Console.WriteLine(intValue);
+            //string testString1 = "một hai ba bốn";
+            //double doubleValue = double.Parse(testString1);
+            //Console.WriteLine(doubleValue);//Báo lỗi
+      
+            string value = "5.5.8";
+            decimal result;
+            bool isValue = decimal.TryParse(value, out result);//Sử dụng TryParse để kiểm tra
+            Console.WriteLine(isValue);
+            Console.WriteLine(result);
             
-            int userInput;
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();// ReadKey() tiếp nhận phím tiếp theo mà user ấn và trả về phím đó
-            Console.WriteLine();
-            Console.WriteLine("Input using Read() -");
-            userInput = Console.Read();//Read() đọc ký tự đầu tiên của input, trả về giá trị ascii;
-            Console.WriteLine("Ascii Value = {0}", userInput);
-            Console.Read();
-            
+           
         }
            
     }
