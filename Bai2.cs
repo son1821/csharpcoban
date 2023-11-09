a7d94a8dd276696dd0900e3594ca2f76a659a04e
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-09 11:50:34 +0700

    add bai tap 2

---
+++ Bai2.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Inport your firtsname and lastname:  ");
+string yourName = Console.ReadLine();
+Console.WriteLine();
+Console.Write("Inport your phonenumber:  ");
+string yourPhoneNumber = Console.ReadLine();
+Console.WriteLine();
+Console.Write("Inport your sex: ");
+string sex = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Name: {0}", yourName);
+Console.WriteLine("PhoneNumber: {0}", yourPhoneNumber);
+Console.WriteLine("Sex: {0}", sex);
+Console.ReadLine();
