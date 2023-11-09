bbf107f83392108f568e75c7b0094054ffb23492
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-09 11:01:31 +0700

    add bai tap 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string inportA;
+Console.Write("Inport The Value of a: ");
+inportA = Console.ReadLine();
+string inportB;
+Console.Write("Inport The Value of b: ");
+inportB = Console.ReadLine();
+int a = int.Parse(inportA);
+int b = int.Parse(inportB);
+int total = a + b;
+Console.WriteLine("Sum of {0} and {1} is: {2}", a, b, total);
+Console.Read();
