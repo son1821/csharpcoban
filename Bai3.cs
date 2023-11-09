dedaf0243215709183f114d8ebb803ca9daa85bf
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-09 12:22:44 +0700

    add bai 3

---
+++ Bai3.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+const double Pi = 3.14;
+Console.Write("Inport raidus:  ");
+string inportRadius = Console.ReadLine();
+double radius = double.Parse(inportRadius);
+double circleArea = radius * radius * Pi;
+Console.WriteLine("Circle Area = {0} ",circleArea);
+Console.Read();
+
+
