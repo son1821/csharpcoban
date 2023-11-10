b1cf04b6f63fd4b5212a7f7723ada84c0b5ab710
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-10 11:42:09 +0700

    Bai15: Exception Handler

---
+++ ExceptionHlandler/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    int a = 10;
+    int c = a / 0;
+
+}
+catch (DivideByZeroException ex1)
+{
+    Console.WriteLine("Cannot divide by zero !");
+}
+
+catch (Exception ex)
+{
+    throw ex;                                          
+}
+finally
+{
+    Console.WriteLine("Oke!");
+}
+Console.ReadLine();
