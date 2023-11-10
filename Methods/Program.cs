919bc66ce977a00cd1122c54853218363b85f92d
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-10 10:11:48 +0700

    Bai14: Phuong thuc

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Methods
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of 4 and 5 = {0}",Add(4,5) );
+            Mutiple(4, 5);
+            Console.ReadLine();
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static void Mutiple(int x, int y)
+        {
+            Console.WriteLine("Mutiple of {0} and {1} = {2} ",x,y,x * y);
+        }
+
+    }
+}
+
