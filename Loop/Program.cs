4add9c0fe2d2bcbed3335cfd67dba7ad0a60145e
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-12 01:51:17 +0700

    Bai19: Vong lap

---
+++ Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//While: xử lý với điều kiện true
+//int a = 1;
+//while (a <= 5)
+//{
+//    Console.WriteLine(a+"");
+//    a++;
+//}
+
+//Do...While: Chạy trước rồi mới kiểm tra, nếu điều kiện true thì lặp tiếp
+//int b;
+//do
+//{
+//    Console.WriteLine("Input b: ");
+//    b=Convert.ToInt32(Console.ReadLine());
+//} while (b<=5);
+//Console.WriteLine("Alright");
+
+//For : xử lý với điều kiện true
+//for (int i = 2; i < 6; i++)
+//{
+//    Console.WriteLine(i + "");
+//}
+
+//Foreach:Xử lý phần tử trên mảng
+int[] Array = new int[10];
+Random  r = new Random();
+for(int i = 0;i < 10; i++)
+{
+    Array[i] = r.Next(1,10);
+}
+Console.WriteLine("Value of element:");
+foreach (int val in Array)
+{
+    Console.WriteLine(val+"");
+}
+
--- SwitchCaseStructure/Program.cs
+++ SwitchCaseStructure/Program.cs
@@ -2,34 +2,44 @@
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-Console.WriteLine("Nhập một số từ 1 đến 8:");
-var day = Console.ReadLine();
-switch (day)
+//Console.WriteLine("Nhập một số từ 1 đến 8:");
+//var day = Console.ReadLine();
+//switch (day)
+//{
+//    case "2":
+//        Console.WriteLine("Thứ hai");
+//        break;
+//    case "3":
+//        Console.WriteLine("Thứ ba");
+//        break;
+//    case "4":
+//        Console.WriteLine("Thứ tư");
+//        break;
+//    case "5":
+//        Console.WriteLine("Thứ năm");
+//        break;
+//    case "6":
+//        Console.WriteLine("Thứ sáu");
+//        break;
+//    case "7":
+//        Console.WriteLine("Thứ bảy");
+//        break;
+//    case "1":
+//    case "8":
+//        Console.WriteLine("Chủ nhật");
+//        break;
+//    default:
+//        Console.WriteLine("Bạn đã nhập sai");
+//        break;
+//}
+Console.WriteLine("Nhập một số:");
+int Number = int.Parse(Console.ReadLine());
+switch (Number %2 )
 {
-    case "2":
-        Console.WriteLine("Thứ hai");
+    case 0:
+        Console.WriteLine("Số chẵn");
         break;
-    case "3":
-        Console.WriteLine("Thứ ba");
-        break;
-    case "4":
-        Console.WriteLine("Thứ tư");
-        break;
-    case "5":
-        Console.WriteLine("Thứ năm");
-        break;
-    case "6":
-        Console.WriteLine("Thứ sáu");
-        break;
-    case "7":
-        Console.WriteLine("Thứ bảy");
-        break;
-    case "1":
-    case "8":
-        Console.WriteLine("Chủ nhật");
-        break;
-    default:
-        Console.WriteLine("Bạn đã nhập sai");
+    default: Console.WriteLine("Số lẻ");
         break;
 }
 Console.Read();
