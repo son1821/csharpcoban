6342dc5be29a5120f3c2dc653db163723742f6c8
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-12 00:54:22 +0700

    Bai18: SwitchCase

---
+++ SwitchCaseStructure/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Nhập một số từ 1 đến 8:");
+var day = Console.ReadLine();
+switch (day)
+{
+    case "2":
+        Console.WriteLine("Thứ hai");
+        break;
+    case "3":
+        Console.WriteLine("Thứ ba");
+        break;
+    case "4":
+        Console.WriteLine("Thứ tư");
+        break;
+    case "5":
+        Console.WriteLine("Thứ năm");
+        break;
+    case "6":
+        Console.WriteLine("Thứ sáu");
+        break;
+    case "7":
+        Console.WriteLine("Thứ bảy");
+        break;
+    case "1":
+    case "8":
+        Console.WriteLine("Chủ nhật");
+        break;
+    default:
+        Console.WriteLine("Bạn đã nhập sai");
+        break;
+}
+Console.Read();
