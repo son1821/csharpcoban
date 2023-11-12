6a9026cb319d053bdd16e9106ea5a3980b93fbcd
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-12 21:16:16 +0700

    Bai21: Lap trinh huong doi tuong

---
+++ OOP/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+        public Person() { }
+        public Person(string name, ushort age) 
+        { 
+        Name = name;
+            Age = age;
+        }
+        public string Name { set; get; }
+        public ushort Age {  set; get; }  
+        public void SayName()
+        {
+            Console.WriteLine(this.Name);
+            Console.WriteLine(this.Age);
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP;
+
+Person person1 = new Person();
+person1.Name = "Nguyen Van A";
+person1.Age = 30;
+person1.SayName();
+Person person2 = new Person("Nguyen Van B", 26);
+person2.SayName();
+Console.Read();
