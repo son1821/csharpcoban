bb0cfda2a9ca2198261306b6702fb1c4403e9176
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-14 23:04:32 +0700

    Ke Thua

---
+++ KeThua/Class1.cs
@@ -0,0 +1,28 @@
+﻿using KeThua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeThua
+{
+    internal class Class1
+    {
+        
+    }
+    class Cat : Animal
+    {
+       public Cat() {
+            Weight = 200;
+            Height = 300;
+            Foot = 4;
+        }
+        public Cat(int w , int h , int f ):base(w,h,f) {
+        
+        
+         }
+        
+    }
+
+}
---
+++ KeThua/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace KeThua
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Infor();
+            Cat cat1 = new Cat(500,600,2);
+            cat1.Infor();
+            Console.ReadLine();
+        }
+       
+
+    }
+    class Animal
+    {
+        protected int Weight;
+        protected int Height;
+        protected int Foot;
+        public void Infor()
+        {
+            Console.WriteLine("Weight :{0}, Height: {1}, Foot: {2}",Weight, Height,Foot);
+        }
+        public Animal() { }
+        public Animal(int w, int h, int f)
+        {
+            Weight = w;
+            Height = h;
+            Foot = f;
+        }
+    }
+}
+
