6d2d8f7d7d953bd7b15089d5803ffea514bb179b
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-15 16:08:21 +0700

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Interface
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IName name = new Animal();
+            name.Name();
+            IName a = new Animal();
+            Console.WriteLine("Số a ={0}", a.a(2));
+            Console.ReadLine();
+        }
+    }
+    interface IName
+    {
+        void Name();
+        int a(int x);
+    }
+    class Animal:IName
+    {
+        public void Name()
+        {
+            Console.WriteLine("Animal");
+        }
+        public int a(int x)
+        {
+            return x;
+        }
+    }
+}
