9186447f25277a016538d90dfd479c56f0841f86
Ngoc Son Ninh <123456nuct@gmail.com>
2023-11-15 11:14:58 +0700

    Tinh Da Hinh, Lop Truu Tuong, Phuong Thuc Thuan Ao

---
+++ TinhDaHinh/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace TinhDaHinh
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+            dog.Name();
+            cat.Name();
+            Human asian = new Asian();
+            asian.Name();
+            Console.ReadLine();
+        }
+    }
+     class Animal
+    {
+        public virtual void Name()
+        {
+            Console.WriteLine("DONG VAT");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Name()
+        {
+            Console.WriteLine("CHO");
+        }
+
+    }
+    class Cat : Animal
+    {
+        public override void Name()
+        {
+            Console.WriteLine("MEO");
+        }
+    }
+    abstract class Human
+    {
+        public abstract void Name();
+    }
+     class Asian : Human
+    {
+        public override void Name()
+        {
+            Console.WriteLine("Nguoi Chau A");
+        }
+    }
+}
